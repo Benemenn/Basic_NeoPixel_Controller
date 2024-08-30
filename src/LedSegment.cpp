9e75217c9e11e5fc8ded7e3bd421e8f6166ba097
Ben Jeff Cloos <ben@cloosefamill.lu>
2024-08-30 16:48:13 +0200

    add different calculations for brightness based on controller

--- src/LedSegment.cpp
+++ src/LedSegment.cpp
@@ -9,20 +9,33 @@ LedSegment::LedSegment(Adafruit_NeoPixel* neopixels, uint16_t startLed, uint16_t
     this->length = length;
 }
 
+
+
 uint32_t LedSegment::calcRGBWithBrightness(uint32_t color, uint8_t brightness){
-    uint8_t b = color << 8;
-    uint8_t g = color << 8;
-    uint8_t r = color << 8;
 
-    // Source: https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
-    float fLuminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-    float fScaleFactor = (float)brightness / fLuminance;
+    uint8_t r = (uint8_t)(color >> 16);
+    uint8_t g = (uint8_t)(color >> 8);
+    uint8_t b = (uint8_t)color;
 
-    uint8_t bPrime = uint8_t((float)b * fScaleFactor);
-    uint8_t gPrime = uint8_t((float)g * fScaleFactor);
-    uint8_t rPrime = uint8_t((float)r * fScaleFactor);
+    #if defined(__AVR__)
 
+        uint8_t rPrime = (r * brightness) >> 8;
+        uint8_t gPrime = (g * brightness) >> 8;
+        uint8_t bPrime = (b * brightness) >> 8;
+
+    #else
+        // Source: https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+        //float may cause problems with small processors
+        float fLuminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+        float fScaleFactor = (float)brightness / fLuminance;
+
+        uint8_t bPrime = uint8_t((float)b * fScaleFactor);
+        uint8_t gPrime = uint8_t((float)g * fScaleFactor);
+        uint8_t rPrime = uint8_t((float)r * fScaleFactor);
+    #endif
+        
     return (bPrime << 16) | (gPrime << 8) | rPrime;
+    
 }
 
 void LedSegment::setOff(){
